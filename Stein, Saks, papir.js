a0934abd52331d0b61692273728c6806dbd16106
Jon Håkon Lia <jh-lia@online.no>
2021-09-26 13:19:35 +0200

    const player choice

--- Stein, Saks, papir.js
+++ Stein, Saks, papir.js
@@ -1,15 +1,12 @@
 // Dette programet skal kjøre eit stei, saks, papir
 
 //const valg = [Stein, Saks, Papir];
-const player_choise = 'Stein'
-const pc = 'pc_choise'
+const player_choice = 'Stein'
+const pc_choice = 'Saks'
 
-const player = choise => {return choise;
-function player_choise(choise) {
-    return choise;
-}};
-
-const my_choise = palyerchoise('Saks');
-const pc_choise = () =>{
+const player = choice => {return choice;
+function player_choice(choice) {
+    return choice};
+    console.log(choice);
 };
-const samnaliknchoise; 
+
