99bd45974ba91dc93fabfa65289fb38e00f62c25
Jon Håkon Lia <jh-lia@online.no>
2021-09-26 14:54:49 +0200

    User input

--- Stein, Saks, papir.js
+++ Stein, Saks, papir.js
@@ -3,15 +3,23 @@
 const choice = ['Stein', 'Saks', 'Papir'];
 const player_choice = 'Stein';
 
+function player_input(choice) {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+       
+      readline.question('Velg Stein, Saks, eller Papir: ', user_choice => {
+        console.log(`Ditt valg er:  ${user_choice}!`);
+        readline.close();
+      });
+    };
+
 function draw_choice(choice) {
-    let pc_choice = choice[Math.floor(Math.random() * choice.lenght)];
+    const pc_choice = choice[Math.floor(Math.random() * choice.length)];
     return pc_choice;
 };
-let pc_choice = draw_choice(choice);
-console.log(pc_choice);
-/*const player = choice => {return choice;
-function player_choice(choice) {
-    return choice};
-    console.log(choice);
-};*/
-
+let pc = draw_choice(choice);
+console.log(pc);
+let uc = player_input(choice);
+console.log(uc);
