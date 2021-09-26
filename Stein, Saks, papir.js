8f51a0c1ff1ba92b0337dfc4627d73c58846a866
Jon Håkon Lia <jh-lia@online.no>
2021-09-26 13:56:07 +0200

    function

--- Stein, Saks, papir.js
+++ Stein, Saks, papir.js
@@ -1,15 +1,17 @@
 // Dette programet skal kjøre eit stei, saks, papir
 
 const choice = ['Stein', 'Saks', 'Papir'];
-const player_choice = 'Stein'
-const pc_choice = 'Saks'
+const player_choice = 'Stein';
 
-const pc_choice () => {
-    return 
+function draw_choice(choice) {
+    let pc_choice = choice[Math.floor(Math.random() * choice.lenght)];
+    return pc_choice;
 };
-const player = choice => {return choice;
+let pc_choice = draw_choice(choice);
+console.log(pc_choice);
+/*const player = choice => {return choice;
 function player_choice(choice) {
     return choice};
     console.log(choice);
-};
+};*/
 
