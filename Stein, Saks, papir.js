5c18a1320116ad3914a991ce55c89a1fcd2205e3
Jon Håkon Lia <jh-lia@online.no>
2021-09-26 15:31:40 +0200

    for loop

--- Stein, Saks, papir.js
+++ Stein, Saks, papir.js
@@ -1,9 +1,10 @@
 // Dette programet skal kjøre eit stei, saks, papir
 
 const choice = ['Stein', 'Saks', 'Papir'];
-const player_choice = 'Stein';
+const player_input = 'Stein';
+const number_of_attempts = 5; 
 
-function player_input(choice) {
+/*function player_input(choice) {
     const readline = require('readline').createInterface({
         input: process.stdin,
         output: process.stdout
@@ -14,12 +15,39 @@ function player_input(choice) {
         readline.close();
       });
     };
-
+*/
 function draw_choice(choice) {
     const pc_choice = choice[Math.floor(Math.random() * choice.length)];
     return pc_choice;
 };
+
+function play(choice, number) {
+    not_finished = true;
+    for (let i = 0; i < number; i++) {
+        let pc = draw_choice(choice)
+        let uc = player_input
+        if (pc  === uc) {
+            console.log(`Bruker: ${uc}, Pc: ${pc} uavgjort.`)
+        }
+        else if (pc === 'Stein' && uc === 'Saks') {
+            console.log(`Bruker: ${uc}, Pc: ${pc} Pc vinner.`)
+        } 
+        else if (pc === 'Saks' && uc === 'Papir') {
+            console.log(`Bruker: ${uc}, Pc: ${pc} Pc vinner.`)
+        }
+        else if (pc === 'Papir' && uc === 'Stein') {
+            console.log(`Bruker: ${uc}, Pc: ${pc} Pc vinner.`)
+        }
+        else {
+            console.log(`Bruker: ${uc}, Pc: ${pc} Bruker vinner.`)
+        }
+            
+    }
+}
+
 let pc = draw_choice(choice);
 console.log(pc);
-let uc = player_input(choice);
+let uc = player_input;
 console.log(uc);
+
+
