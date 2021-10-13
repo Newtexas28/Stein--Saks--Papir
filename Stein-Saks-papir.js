25034f0c3537a10bdad4d28a1c8d500db9856c02
Jon Håkon Lia <jh-lia@online.no>
2021-10-13 22:01:27 +0200

    fiksa bug

--- Stein-Saks-papir.js
+++ Stein-Saks-papir.js
@@ -6,13 +6,7 @@ function player_input(choice)
 {
     let input = prompt("Hva velger du [stein, saks eller papir]? ");
     input = input.toLowerCase();
-        if (input === 'stein' || input === 'saks' || input === 'papir' || input === 'bombe') 
-        {
-            return input;
-        }
-        else {
-            console.log('Error: Brukervalget er ugyldig')
-    };
+    return input;
 };
 
 function draw_choice(choice) {
@@ -26,35 +20,34 @@ function play(choice, number, Cheet_code) {
     for (let i = 0; i < number; i++) {
         let pc = draw_choice(choice)
         const uc = player_input();
-        console.log('Og tallet er: ' + i);
-    if (uc === 'stein' || uc === 'saks' || uc === 'papir' || uc === 'bombe') {
+        if (uc === 'stein' || uc === 'saks' || uc === 'papir' || uc === 'bombe') {
             //console.log('Inputen i progremet er ugyldig')
-        if (uc === Cheet_code) {
-            console.log('Bruker vant med Cheet code');
-            i = number;
+            if (uc === Cheet_code) {
+                console.log('Bruker vant med Cheet code');
+                break;
+            }
+            else if (pc === uc) {
+                console.log(`Forsøk: ${i} Bruker: ${uc}, Pc: ${pc} uavgjort.`);
+            }
+            else if (pc === 'stein' && uc === 'saks') {
+                console.log(`Forsøk: ${i} Bruker: ${uc}, Pc: ${pc} Pc vinner.`);
+            } 
+            else if (pc === 'saks' && uc === 'papir') {
+                console.log(`Forsøk: ${i} Bruker: ${uc}, Pc: ${pc} Pc vinner.`);
+            }
+            else if (pc === 'papir' && uc === 'stein') {
+                console.log(`Forsøk: ${i} Bruker: ${uc}, Pc: ${pc} Pc vinner.`);
+            }
+            else {  
+                console.log(`Forsøk: ${i} Bruker: ${uc}, Pc: ${pc} Bruker vinner.`);
+            }
+        }
+        else {
+            console.log('Error: Brukervalget er ugyldig'); 
             break;
-        }
-        else if (pc === uc) {
-            console.log(`Forsøk: ${i} Bruker: ${uc}, Pc: ${pc} uavgjort.`);
-        }
-        else if (pc === 'Stein' && uc === 'Saks' || uc === 'Saks' && pc === 'Stein') {
-            console.log(`Forsøk: ${i} Bruker: ${uc}, Pc: ${pc} Pc vinner.`);
-        } 
-        else if (pc === 'Saks' && uc === 'Papir' || uc === 'Papir' && pc === 'Saks') {
-            console.log(`Forsøk: ${i} Bruker: ${uc}, Pc: ${pc} Pc vinner.`);
-        }
-        else if (pc === 'Papir' && uc === 'Stein' || uc === 'Stein' && pc === 'Papir' ) {
-            console.log(`Forsøk: ${i} Bruker: ${uc}, Pc: ${pc} Pc vinner.`);
-        }
-        else {  
-            console.log(`Forsøk: ${i} Bruker: ${uc}, Pc: ${pc} Bruker vinner.`);
-        }
-    }
-    else {
-        break;
-    }   
-    }
-}
+        };   
+    };
+};
 // Her starter hovud programent 
 function main() {
     const choice = ['stein', 'saks', 'papir'];
@@ -62,10 +55,9 @@ function main() {
     const Cheet_code = 'bombe';
 
     // Siden vi henger ut brukerens valg inne funksjonen er det ikke nødvendig å sende noe inn her.
-    play(choice, number_of_attempts, Cheet_code, 'har ingen betydning');
+    play(choice, number_of_attempts, Cheet_code);
     /*
-     * Slik koden er nå kan jeg skrive f.eks. "fisk" og finne hele tiden selv om
-     * "fisk" ikke er en cheetcode. Hvorfor det?
+     * 
      * Kan du skrive logikken i play() anderledes?
      * Hvordan sikrer du at brukeren holder seg til spillereglene?
      */ 
